661fa5a89f466467b56017f5f196cb926601e30b
Kelsey S <kswanson426@gmail.com>
2017-01-06 13:25:12 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+function BlockMove(event) {
+  event.preventDefault() ;
+ }
+
+;(function($) {
+  $.extend({
+    stayInWebApp: function(selector) {
+      if(("standalone" in window.navigator) && window.navigator.standalone) {
+        if(!selector) {
+          selector = 'a';
+        }
+        $("body").delegate(selector,"click",function(event) {
+        
+       
+          if($(this).attr("target") == undefined || $(this).attr("target") == "" || $(this).attr("target") == "_self") {
+           
+            var dest = $(this).attr("href");
+            if(!dest.match(/^http(s?)/g)) {
+              event.preventDefault();
+              self.location = dest;
+            }
+          }
+        });
+      }
+    } //end stayInWebApp func
+  });
+})( jQuery );
+
+  $(document).ready(function() {
+      $(function() {
+          $.stayInWebApp('a.stay');
+      });
+  });
+
+
+
+
+document.body.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+});
+
+
